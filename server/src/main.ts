7225624778c02e35cba2a04c733a1724de049e9f
frankly034 <franklynugege@gmail.com>
2023-08-18 10:02:45 +0100

    BE - add helmet and compression

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,10 +2,14 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ConfigService } from '@nestjs/config';
 import { ValidationPipe } from '@nestjs/common';
+import helmet from 'helmet';
+import * as compression from 'compression';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
 
+  app.use(compression());
+  app.use(helmet());
   app.useGlobalPipes(new ValidationPipe());
 
   const configService = app.get(ConfigService);
