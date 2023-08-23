c7c8624075b17be40199567ceb349fb2f11703a9
frankly034 <franklynugege@gmail.com>
2023-08-23 04:16:22 +0100

    FE - setup storybook

---
+++ client/components/title/Title.stories.tsx
@@ -0,0 +1,18 @@
+import type { Meta, StoryObj } from "@storybook/react";
+
+import Title from "./Title";
+
+const meta: Meta<typeof Title> = {
+  title: "Components/Title",
+  component: Title,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Title>;
+
+export const Default: Story = {
+  args: {
+    children: "Gift a friend this holiday",
+  },
+};
